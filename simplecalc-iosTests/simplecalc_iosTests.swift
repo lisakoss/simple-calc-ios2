2295036d89414dd4b12178cb0c4fccd4e58ee37e
Lisa Koss <lisakoss@uw.edu>
2018-01-31 16:38:14 -0800

    Initial Commit

---
+++ simplecalc-iosTests/simplecalc_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  simplecalc_iosTests.swift
+//  simplecalc-iosTests
+//
+//  Created by Lisa Koss on 1/31/18.
+//  Copyright © 2018 Lisa Koss. All rights reserved.
+//
+
+import XCTest
+@testable import simplecalc_ios
+
+class simplecalc_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
